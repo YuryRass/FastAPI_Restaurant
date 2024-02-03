3033bcf5e3fbed7a7a48f74181f3b804aae6a9de
Yurii <yurchik1997yurchik@gmail.com>
2024-02-03 07:51:06 +0300

    initial commit

---
+++ app/database/database.py
@@ -0,0 +1,34 @@
+import uuid
+
+from sqlalchemy import UUID, NullPool
+from sqlalchemy.ext.asyncio import AsyncEngine, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase, Mapped, declared_attr, mapped_column
+
+from app.config import settings
+
+if settings.MODE == 'TEST':
+    DATABASE_URL = settings.TEST_DATABASE_URL
+    DATABASE_PARAMS = {'poolclass': NullPool}
+else:
+    DATABASE_URL = settings.DATABASE_URL
+    DATABASE_PARAMS = {}
+
+async_engine: AsyncEngine = create_async_engine(DATABASE_URL, **DATABASE_PARAMS)
+
+async_session: async_sessionmaker = async_sessionmaker(
+    async_engine, expire_on_commit=False
+)
+
+
+class Base(DeclarativeBase):
+    __abstract__ = True
+
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        return cls.__name__.lower()
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        default=uuid.uuid4,
+    )
