9cd40821fcaa2994ceb882e7bcee1fa73a49f8c2
Yurii <yurchik1997yurchik@gmail.com>
2024-02-11 16:27:56 +0300

    updated

--- app/tasks/tasks.py
+++ app/tasks/tasks.py
@@ -1,7 +1,7 @@
+import logging
 from app.tasks.celery import celery
 from app.tasks.db_updater import DBUpdater
 
-# from app.tasks.excel_writer import ExcelWorker
 from app.utils.excel_reader import ExcelReader
 
 
@@ -9,20 +9,9 @@ from app.utils.excel_reader import ExcelReader
 def update_db() -> None:
     """Обновление данных в БД из удаленного excel файла."""
     try:
-        print('Запуск задачи обновления БД')
         er = ExcelReader()
         menus_schema = er.get_menus()
         updater = DBUpdater(menus_schema)
         updater.run()
-    except Exception as error:
-        print(f'Ошибка при обновлении БД: {error}')
-
-# @celery.task
-# def write_excel() -> None:
-#     """Запись данных в локальный excel файл."""
-#     try:
-#         print('Запуск задачи записи в Excel')
-#         ew = ExcelWorker()
-#         ew.write_data()
-#     except Exception as error:
-#         print(f'Ошибка при записи в Excel: {error}')
+    except Exception as e:
+        logging.error(e)
