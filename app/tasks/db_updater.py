4e6b01821088f84bdf90473711c58662ff6db7f4
Yurii <yurchik1997yurchik@gmail.com>
2024-02-11 12:27:13 +0300

    fixing

--- app/tasks/db_updater.py
+++ app/tasks/db_updater.py
@@ -1,4 +1,5 @@
 import uuid
+from enum import Enum
 from typing import Any
 
 import httpx
@@ -10,6 +11,12 @@ from app.submenu.model import Submenu
 from app.utils.json_shemas import JsonDish, JsonMenu, JsonSubmenu, JsonType
 
 
+class DBModel(str, Enum):
+    dish = 'dish'
+    menu = 'menu'
+    submenu = 'submenu'
+
+
 class DBUpdater:
     """Обновление данных в БД после чтения excel файла."""
 
@@ -17,9 +24,9 @@ class DBUpdater:
         self.parser_data = parser_data
         self.base_url = settings.APP_LINK
         self.models_dict: dict[str, Any] = {
-            'dish': Dish,
-            'menu': Menu,
-            'submenu': Submenu,
+            DBModel.dish: Dish,
+            DBModel.menu: Menu,
+            DBModel.submenu: Submenu,
         }
 
     def get_models_id_from_db(self, model: str, **kwargs) -> list[uuid.UUID]:
@@ -46,79 +53,12 @@ class DBUpdater:
 
         httpx.post(url, json=data)
 
-    # def post_menu(self, menu: JsonMenu) -> None:
-    #     """Запостить новое меню в базу."""
-    #     url = self.base_url + Menu.LINK
-    #     data = {
-    #         "id": str(menu.id),
-    #         "title": menu.title,
-    #         "description": menu.description,
-    #     }
-    #     httpx.post(url, json=data)
-
-    # def post_submenu(
-    #     self,
-    #     submenu: JsonSubmenu,
-    #     menu_id: uuid.UUID,
-    # ) -> None:
-    #     """Запостить новое подменю в базу."""
-    #     url = self.base_url + Submenu.LINK.format(menu_id=menu_id)
-    #     data = {
-    #         "id": str(submenu.id),
-    #         "title": submenu.title,
-    #         "description": submenu.description,
-    #     }
-    #     httpx.post(url, json=data)
-
-    # def post_dish(
-    #     self,
-    #     dish: JsonDish,
-    #     submenu_id: uuid.UUID,
-    #     menu_id: uuid.UUID,
-    # ) -> None:
-    #     """Запостить новое блюдо в базу."""
-    #     url = self.base_url + Dish.LINK.format(
-    #         menu_id=menu_id,
-    #         submenu_id=submenu_id,
-    #     )
-    #     data = {
-    #         "id": str(dish.id),
-    #         "title": dish.title,
-    #         "description": dish.description,
-    #         "price": str(dish.price),
-    #     }
-    # httpx.post(url, json=data)
-
     def add_models_batch(
         self, model: str, models_data: list[JsonType], **kwargs
     ) -> None:
         """Добавить новые данные моделей в БД списком."""
         for model_data in models_data:
             self.add_model_data(model, model_data, **kwargs)
-
-    # def post_submenus_batch(
-    #     self, submenus: list[JsonSubmenu], menu_id: uuid.UUID
-    # ) -> None:
-    #     """Запостить новые подменю в базу списком."""
-    #     for submenu in submenus:
-    #         self.post_submenu(
-    #             submenu=submenu,
-    #             menu_id=menu_id,
-    #         )
-
-    # def post_dishes_batch(
-    #     self,
-    #     dishes: list[JsonDish],
-    #     submenu_id: uuid.UUID,
-    #     menu_id: uuid.UUID,
-    # ) -> None:
-    #     """Запостить новые блюда в базу списком."""
-    #     for dish in dishes:
-    #         self.post_dish(
-    #             dish=dish,
-    #             submenu_id=submenu_id,
-    #             menu_id=menu_id,
-    #         )
 
     def patch_menu(self, menu: JsonMenu) -> None:
         """Обновить данные о меню в базе."""
@@ -246,14 +186,14 @@ class DBUpdater:
         """Проверить состояние блюд в базе и привести
         в соответствие с файлом."""
         dishes_id = self.get_models_id_from_db(
-            'dish',
+            DBModel.dish,
             menu_id=menu_id,
             submenu_id=submenu_id,
         )
         for dish in dishes:
             if dish.id not in dishes_id:
                 self.add_model_data(
-                    'dish',
+                    DBModel.dish,
                     dish,
                     submenu_id=submenu_id,
                     menu_id=menu_id,
@@ -275,12 +215,12 @@ class DBUpdater:
     ) -> None:
         """Проверить состояние подменю в базе и привести
         в соответствие с файлом."""
-        submenus_id = self.get_models_id_from_db('submenu', menu_id=menu_id)
+        submenus_id = self.get_models_id_from_db(DBModel.submenu, menu_id=menu_id)
         for submenu in submenus:
             if submenu.id not in submenus_id:
-                self.add_model_data('submenu', submenu, menu_id=menu_id)
+                self.add_model_data(DBModel.submenu, submenu, menu_id=menu_id)
                 self.add_models_batch(
-                    'dish',
+                    DBModel.dish,
                     submenu.dishes,
                     submenu_id=submenu.id,
                     menu_id=menu_id,
@@ -309,15 +249,15 @@ class DBUpdater:
         menus_id = self.get_models_id_from_db('menu')
         for menu in self.parser_data:
             if menu.id not in menus_id:
-                self.add_model_data('menu', menu)
+                self.add_model_data(DBModel.menu, menu)
                 self.add_models_batch(
-                    'submenu',
+                    DBModel.submenu,
                     menu.submenus,
                     menu_id=menu.id,
                 )
                 for submenu in menu.submenus:
                     self.add_models_batch(
-                        'dish',
+                        DBModel.dish,
                         submenu.dishes,
                         submenu_id=submenu.id,
                         menu_id=menu.id,
